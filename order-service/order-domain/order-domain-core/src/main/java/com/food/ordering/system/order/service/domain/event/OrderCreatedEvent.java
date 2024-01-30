46c4068d396cffc32624f18b315f0cd161efcf67
Karina Sofia Medina <kamedina@expediagroup.com>
2024-01-29 23:48:25 -0300

    more changes
    Signed-off-by: Karina Sofia Medina <kamedina@expediagroup.com>

--- order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderCreatedEvent.java
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/event/OrderCreatedEvent.java
@@ -6,7 +6,7 @@ import com.food.ordering.system.order.service.domain.entity.Order;
 
 import java.time.ZonedDateTime;
 
-public class OrderCreatedEvent extends OrderEvent {
+public class OrderCreatedEvent extends OrderEvent implements DomainEvent {
 
     public OrderCreatedEvent(Order order, ZonedDateTime createdAt) {
         super(order, createdAt);
